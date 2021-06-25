5485c1d08f780bcd3a880293123d343d99e94950
Bob Currier <robertdcurrier@gmail.com>
2021-06-25 13:36:11 -0400

    split flow and wq stations as data and url are different

--- dbhydro_fetch_data.py
+++ dbhydro_fetch_data.py
@@ -51,8 +51,8 @@ def get_config():
     return config
 
 
-def fetch_station_data(station, config, logger):
-    """Name:       fetch_station_data.
+def fetch_wq_data(station, config, logger):
+    """Name:       fetch_wq_data.
 
     Author:     robertdcurrier@gmail.com
     Created:    2020-07-07
@@ -60,25 +60,25 @@ def fetch_station_data(station, config, logger):
     Notes:      Gets base URL from configuration file and uses requests to
     get station data from the DBHYDRO RESTful API
     """
-    url = config['dbhydro_url']
+    url = config['dbhydro_wq_url']
     station_id = "station_id='%s'" % station
     url = url.replace('station_id=', station_id)
 
-    logger.info('fetch_station_data(): Fetching %s' % station)
+    logger.info('fetch_wq_data(): Fetching %s' % station)
     try:
         resp = requests.get(url)
     except requests.ConnectionError as error:
-        logger.info("fetch_station_data() Failed with error %s" % error)
+        logger.info("fetch_wq_data() Failed with error %s" % error)
     # Consider any status other than 2xx an error
     if not resp.status_code // 100 == 2:
-        logger.info('fetch_station_data(): Unexpected response {}'.format(resp))
+        logger.info('fetch_wq_data(): Unexpected response {}'.format(resp))
         return False
     data_frame = pd.read_csv(io.StringIO(resp.content.decode('utf-8')))
     return data_frame
 
 
-def extract_station_data(station, data_frame, config, logger):
-    """Name:       extract_station_data.
+def extract_wq_station_data(station, data_frame, config, logger):
+    """Name:       extract_wq_station_data.
 
     Author:     robertdcurrier@gmail.com
     Created:    2020-07-08
@@ -86,14 +86,29 @@ def extract_station_data(station, data_frame, config, logger):
     Notes:      Iterates over variables in config file and extracts readings
     from data frame. Added station so we can debug logger.info station name.
     """
-    logger.info("extract_station_data(%s)" % station)
+    logger.info("extract_wq_station_data(%s)" % station)
     dbhvars = config['variables']
     new_df = data_frame[dbhvars].copy().dropna()
     return new_df
 
 
-def process_stations(logger):
-    """Name:    process_stations
+def extract_flow_station_data(station, data_frame, config, logger):
+    """Name:       extract_flow_station_data.
+
+    Author:     robertdcurrier@gmail.com
+    Created:    2021-06-25
+    Modified:   2021-06-25
+    Notes:      Iterates over variables in config file and extracts readings
+    from data frame. Added station so we can debug logger.info station name.
+    """
+    logger.info("extract_flow_station_data(%s)" % station)
+    dbhvars = config['variables']
+    new_df = data_frame[dbhvars].copy().dropna()
+    return new_df
+
+
+def process_wq_stations(logger):
+    """Name:    process_wq_stations
 
     Author:     robertdcurrier@gmail.com
     Created:    2020-07-07
@@ -101,19 +116,31 @@ def process_stations(logger):
     Notes:      Modified for use by Ashley Brereton
     """
     config = get_config()
-    logger.info('processing stations...')
-    for site in config['stations']:
+    logger.info('processing water quality stations...')
+    for site in config['wq_stations']:
         station = site['station']
-        data_frame = fetch_station_data(station, config, logger)
-        extracted_df = extract_station_data(station, data_frame, config, logger)
+        data_frame = fetch_wq_data(station, config, logger)
+        extracted_df = extract_wq_station_data(station, data_frame, config, logger)
         if len(extracted_df) > 0:
             filename = "reports/%s.csv" % station
-            logger.info('process_stations(): Writing %s' % filename)
+            logger.info('process_wq_stations(): Writing %s' % filename)
             csv = extracted_df.to_csv(filename, index=False)
         else:
-            logger.warning('process_stations(): %s returned no data.' % station)
+            logger.warning('process_wq_stations(): %s returned no data.' % station)
+
+
+def process_flow_stations(logger):
+        """Name:    process_flow_stations
+
+        Author:     robertdcurrier@gmail.com
+        Created:    2021-06-25
+        Modified:   2021-06-25
+        Notes:      Split process stations into wq and flow
+        """
+        pass
 
 
 if __name__ == '__main__':
     logger = config_logging()
-    process_stations(logger)
+    process_wq_stations(logger)
+    process_flow_stations(logger)
